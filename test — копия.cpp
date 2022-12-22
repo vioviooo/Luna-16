765a0fc79a3378fe57bd946cc8c1b86f4b137844
Victoria Brovina <vika_brovina@mail.ru>
2022-12-22 18:42:11 +0300

    first commit

---
+++ test — копия.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip> 
+#include <string>
+#include <queue>
+#include <set>
+#include <fstream>
+#include <ctime>
+#include <random>
+#include <bitset>
+#include <array>
+
+using namespace std;
+
+const long long MOD = 1e9 + 7;
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+
+template <typename T>
+
+using pair2 = pair <T, T>;
+using pii = pair <int, int>;
+using pli = pair <ll, int>;
+using pll = pair <ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define fastboi ios::sync_with_stdio(false); cin.tie(0);
+
+clock_t startTime;
+double timing() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+const ll INF = 1e18;
+
+int main() { 
+    
+    fastboi
+    
+    ll a, b;
+    cin >> a >> b;
+
+    ll res = a + b;
+
+    cout << res << '\n';
+     
+    //cout << endl << timing();
+    
+    return 0;
+}
